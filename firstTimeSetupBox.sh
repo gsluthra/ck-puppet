ffc100ea711c02fe147de725d066145ac7c24901
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-05-03 18:43:40 +0530

    Removed the commented out line since its now being executed by puppet as an exec command

--- firstTimeSetupBox.sh
+++ firstTimeSetupBox.sh
@@ -5,12 +5,8 @@
 sudo puppet module install puppetlabs-postgresql
 sudo puppet module install puppetlabs-apache
 
-# Add a postgres9.3 repo (todo: Move to puppet manifest)
-# sudo yum --assumeyes localinstall http://yum.postgresql.org/9.3/redhat/rhel-6-x86_64/pgdg-centos93-9.3-1.noarch.rpm
-
 
 # Final Report
 echo -------------------------------------------------------------------------------
-sudo yum repolist
 sudo puppet module list
 echo -------------------------------------------------------------------------------
